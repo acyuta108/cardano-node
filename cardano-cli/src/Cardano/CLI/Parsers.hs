75078e613658bc5dbcf6599ef9c932c7c114db88
John Ky <john.ky@iohk.io>
2021-07-22 11:40:16 +1000

    Fix up build

--- cardano-cli/src/Cardano/CLI/Parsers.hs
+++ cardano-cli/src/Cardano/CLI/Parsers.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Cardano.CLI.Parsers
@@ -19,6 +20,8 @@ import           Prettyprinter.Render.Util.SimpleDocTree
 
 import qualified Data.Text as T
 import qualified Options.Applicative as Opt
+import qualified System.Environment as IO
+import qualified System.IO.Unsafe as IO
 
 command' :: String -> String -> Parser a -> Mod CommandFields a
 command' c descr p =
@@ -41,20 +44,35 @@ pref = Opt.prefs $ mempty
   <> helpHangUsageOverflow 10
   <> helpRenderHelp customRenderHelp
 
+cliHelpTraceEnabled :: Bool
+cliHelpTraceEnabled = IO.unsafePerformIO $ do
+  mValue <- IO.lookupEnv "CLI_HELP_TRACE"
+  return $ mValue == Just "1"
+{-# NOINLINE cliHelpTraceEnabled #-}
+
 -- | Convert a help text to 'String'.
 customRenderHelp :: Int -> ParserHelp -> String
 customRenderHelp cols
   = T.unpack
-  . ("<html>\n" <>)
-  . ("<body>\n" <>)
-  . ("<pre>\n" <>)
-  . (<> "\n</html>")
-  . (<> "\n</body>")
-  . (<> "\n</pre>")
-  . renderSimplyDecorated id (\(AnnTrace _ name) x -> "<span name=" <> show name <> ">" <> x <> "</span>")
+  . wrapper
+  . renderSimplyDecorated id renderElement
   . treeForm
   . layoutSmart (LayoutOptions (AvailablePerLine cols 1.0))
   . helpText
+  where
+    renderElement = if cliHelpTraceEnabled
+      then \(AnnTrace _ name) x -> "<span name=" <> show name <> ">" <> x <> "</span>"
+      else flip const
+    wrapper = if cliHelpTraceEnabled
+      then id
+        . ("<html>\n" <>)
+        . ("<body>\n" <>)
+        . ("<pre>\n" <>)
+        . (<> "\n</html>")
+        . (<> "\n</body>")
+        . (<> "\n</pre>")
+      else id
+    
 
 parseClientCommand :: Parser ClientCommand
 parseClientCommand =
